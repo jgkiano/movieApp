4a5aa8f5e7c0f830106d517eb28ef1baa3e9272e
Julius Kiano <juliuskiano@Juliuss-MacBook-Pro.local>
2017-03-10 08:49:26 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Julius Kiano on 3/10/17.
+//  Copyright © 2017 Julius Kiano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
